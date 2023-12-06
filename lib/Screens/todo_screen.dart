225220e0224fd5e3812f3a5cd93372e43bb595d6
Sena Kim <sena0110@hufs.ac.kr>
2023-12-05 17:57:17 -0800

    업로드

--- lib/Screens/todo_screen.dart
+++ lib/Screens/todo_screen.dart
@@ -1,1 +1,56 @@
-class TodoScreen
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+import '../models/todo.dart';
+
+class TodoScreen extends StatefulWidget {
+  const TodoScreen({super.key});
+
+  @override
+  State<TodoScreen> createState() => _TodoScreenState();
+}
+
+class _TodoScreenState extends State<TodoScreen> {
+  TextEditingController textEditingController = new TextEditingController();
+
+
+  @override
+  void dispose() {
+    textEditingController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Todo Screen'),
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: textEditingController,
+                ),
+              ),
+              TextButton(
+                onPressed: () => addTodo(Todo(textEditingController.text)),
+                child: Text('추가'),
+              ),
+            ],
+          ),
+
+        ],
+      ),
+    );
+  }
+
+  void addTodo(Todo todo) {
+    FirebaseFirestore.instance
+        .collection('todo')
+        .add({'title':todo.title,'isDone':todo.isDone});
+    textEditingController.text = '';
+  }
+}
---
+++ lib/models/todo.dart
@@ -0,0 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+class Todo {
+  String title;
+  bool isDone;
+
+  Todo(this.title, {this.isDone = false});
+}
