05a315acd09f28459abd5e48586b601225eaed80
Sena Kim <sena0110@hufs.ac.kr>
2023-12-05 17:59:56 -0800

    업로드

--- lib/Screens/test_widget_screen.dart
+++ lib/Screens/test_widget_screen.dart
@@ -1,55 +1,34 @@
-import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:mobile_project/Screens/nav_test_screen.dart';
-import 'package:mobile_project/Screens/test_widget_screen.dart';
-import 'Screens/test_widget.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'firebase_options.dart';
+import 'package:mobile_project/Screens/test_widget.dart';
 
-void main() async {
-
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  dotenv.load(fileName: ".env");
-
-  runApp(
-    const MaterialApp(
-      home: HomeWidget(),
-    ),
-  );
-}
-
-class HomeWidget extends StatefulWidget {
-  const HomeWidget({super.key});
+class TestWidgetScreen extends StatefulWidget {
+  const TestWidgetScreen({super.key});
 
   @override
-  State<HomeWidget> createState() => _HomeWidgetState();
+  State<TestWidgetScreen> createState() => _TestWidgetScreenState();
 }
 
-class _HomeWidgetState extends State<HomeWidget> {
+class _TestWidgetScreenState extends State<TestWidgetScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home'),
+        title: Text('Test Widget Screen'),
       ),
-      body: Center(
-        child: TextButton(
-          child: const Text(
-            'Go To Nav Test Screen',
-            style: TextStyle(fontSize: 26),
+      body: Column(
+        children: [
+          TestWidget(),
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text(
+              'Go To Back',
+              style: TextStyle(fontSize: 26),
+            ),
           ),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => const NavTestScreen(),
-              ),
-            );
-          },
-        ),
+        ],
       ),
     );
   }
